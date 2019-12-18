ead2b5402e2323f2a403694b8df6b9e74d8b6c19
Cristina Murillo <murillocam96@gmail.com>
2019-12-17 19:38:17 -0500

    image selection functional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
 import ColorChannels from './components/ColorChannels'
 import ImageAnalyzer from './components/ImageAnalyzer'
-import ImageRender from './components/ImageRender'
-import ImageSelection, {defaultImg} from './components/ImageSelection';
-import gaga from './images/gaga.jpg'
+import ImageRender, {defaultImg} from './components/ImageRender'
 
 const App = () => {
   const [imageData, setImageData] = useState([])
   const [userRect, setUserRect] = useState({x: 0, y: 0, width: 100, height: 100})
-  const [userSelection, setUserSelection] = useState(gaga)
+  const [userSelection, setUserSelection] = useState(defaultImg)
   return (
     <div className="App">
         <ImageRender setUserRect={setUserRect} setUserSelection={setUserSelection} userSelection={userSelection}/>
--- src/components/ImageRender.js
+++ src/components/ImageRender.js
@@ -6,8 +6,9 @@ import gaga from '../images/gaga.jpg'
 import lizzo from '../images/lizzo.jpg'
 
 
-const ImageRender = ({setUserRect, setUserSelection, userSelection}) => {
+const images = [billy,ezra,gaga,lizzo]
 
+const ImageRender = ({setUserRect, setUserSelection, userSelection}) => {
     const [userMousePosition, setUserMousePosition] = useState({top: '100px', left: '100px'})
     
     const moveHandler = (event) => {
@@ -26,17 +27,42 @@ const ImageRender = ({setUserRect, setUserSelection, userSelection}) => {
             height: 150 * ratio
         })
     }
+    const newSelectionHandler = (event) => {
+        let currentIndex = images.findIndex(image => image === userSelection)
+        if (currentIndex < images.length - 1) {
+            currentIndex += 1
+        } else {
+            currentIndex = 0
+        }
+        setUserSelection(images[currentIndex])
+    }
+
+    const previousSelectionHandler = (event) => {
+        let currentIndex = images.findIndex(image => image === userSelection)
+        if (currentIndex > 0) {
+            currentIndex -= 1
+        } else {
+            currentIndex = 0
+        }
+        setUserSelection(images[currentIndex])
+        
+    }
+
     return(
         <div className="img-render">
-            <img src={NextIcon} onClick={() => setUserSelection(lizzo)} className="next-icon left" alt="next arrow"></img>            
+            <img src={NextIcon} onClick={previousSelectionHandler} className="next-icon left" alt="next arrow"></img>            
             <div className="img-wrapper">
                 <img className="selected-img" src={userSelection} onMouseMove={moveHandler} alt="met gala" width='100%' ></img>
                 <div className="mouse-rect" style={userMousePosition}></div>
             </div>
-            <img src={NextIcon} onClick={() => setUserSelection(lizzo)} className="next-icon" alt="next arrow"></img>
+            <img src={NextIcon} onClick={newSelectionHandler} className="next-icon" alt="next arrow"></img>
         </div>
     )
 }
 
+export const defaultImg = () => {
+    const randomIndex = Math.floor(Math.random() * (images.length+1))
+    return images[randomIndex]
+}
 
 export default ImageRender
